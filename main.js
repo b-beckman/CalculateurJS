89792c4e0aecf2e2f77d973a9ced5ee130372a45
b-beckman <buessleo.pro@gmail.com>
2022-11-22 13:16:48 +0100

    Store note in the right object

--- main.js
+++ main.js
@@ -6,72 +6,82 @@ let Objects = {
     semestre1: [],
   },
   tabl3: {
-    maths: {
-      semestre1: [],
-      semestre2: [],
-      semestre3: [],
-    },
-      anglais: {
-        semestre1: [],
-        semestre2: [],
-        semestre3: [],
-        semestre4: [],
-        semestre5: [],
-      },      
-      }, 
-      tabl4: {
-      semestre1: [],
-      semestre2: [],
-      semestre3: [],
-      semestre4: [],
-    },
-    tabl5:{
-      semestre1: [],
-    }
-}
-
-let inputs
-let grade
-let tablo
-let descri
+    mathSemestre1: [],
+    mathSemestre2: [],
+    mathSemestre3: [],
+    anglaisSemestre1: [],
+    anglaisSemestre2: [],
+    anglaisSemestre3: [],
+    anglaisSemestre4: [],
+    anglaisSemestre5: [],
+  },
+  tabl4: {
+    cultGSemestre1: [],
+    cultGSemestre2: [],
+    cultGSemestre3: [],
+    cultGSemestre4: [],
+    cultGSemestre5: [],
+    cultGSemestre6: [],
+  },
+  tabl5: {
+    semestre1: [],
+  },
+};
+//Loop for index
 for (var i = 1; i <= 5; i++) {
   getButton(i);
 }
-
+//Main function
 function getButton(index) {
-  inputs = document.getElementById("addButton-" + index);
+  let inputs = document.getElementById("addButton-" + index);
   inputs.addEventListener("click", function () {
-    tablo = document.getElementById("tablo-" + index);
-    grade = parseFloat(document.getElementById("noteInput-" + index).value);
-    descri = parseFloat(document.getElementById("descri-" + index).value);
-    if(!isNaN(grade, descri)){
-    console.log("jai click" + " " + index);
-    var row = tablo.insertRow(0)
-    console.log(row);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    cell1.innerHTML = grade;
-    cell2.innerHTML = descri;
-    }else{
-    alert("Votre note n'est pas valable.")  
+    let grade = parseFloat(document.getElementById("noteInput-" + index).value);
+    //Condition to add the note or not
+    if (grade > 6) {
+      alert("Votre note n'est pas valable.");
+      return 0;
     }
-}); 
+    if (!isNaN(grade)) {
+      if (index == 3 || index === 4) {
+        let sems = document.getElementById("dropdown-" + index).value;
+        let tablo = document.getElementById(sems);
+        Objects["tabl" + index][sems].push(grade);
+        console.log(Objects["tabl" + index][sems]);
+        let row = tablo.insertRow(0);
+        let cell1 = row.insertCell(0);
+        cell1.innerHTML = grade;
+      } else {
+        let descri = parseFloat(document.getElementById("descri-" + index).value);
+        let tablo = document.getElementById("tablo-" + index);
+        Objects["tabl" + index].semestre1.push(grade);
+        console.log(Objects["tabl" + index].semestre1);
+        let row = tablo.insertRow(0);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        cell1.innerHTML = grade;
+        cell2.innerHTML = descri;
+      }
+    } else {
+      alert("Votre note n'est pas valable.");
+    }
+  });
 }
-
 // function getTablo(index) {
 // var table = document.getElementById("tablo" + index);
 // var row = table.insertRow([0]);
 // getbutton(index, row);
 // }
-//Bastien 
+//Bastien
 // const buttons = document.querySelectorAll('.addButton')
 // for (const button of buttons) {
 //   button.addEventListener('click', (e) => {
 //     const inputs = e.currentTarget.parentElement.parentElement.querySelectorAll('input')
 //   })}
+// s.push(Object.keys(Objects.tabl + index))
+// console.log(Object.keys(Objects.tabl + index));
+/* <div id="testBN"></div>;
 
-
-
-
-    // s.push(Object.keys(Objects.tabl + index))
-    // console.log(Object.keys(Objects.tabl + index));
+function updateViewFromDatas(data) {
+  for (const [key, value] of Object.entries(data)) {
+    console.log(key);
+    console.log(value);}} */

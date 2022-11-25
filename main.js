7033864865914735111f22cc4ad9bc681762bacd
b-beckman <buessleo.pro@gmail.com>
2022-11-25 10:54:45 +0100

    Latest version

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let Objects = {
+let Notes = {
   tabl1: {
     semestre1: [],
   },
@@ -6,7 +6,7 @@ let Objects = {
     semestre1: [],
   },
   tabl3: {
-    mathSemestre1: [],
+    mathSemestre1: [3, 2, 1, 5],
     mathSemestre2: [],
     mathSemestre3: [],
     anglaisSemestre1: [],
@@ -27,11 +27,31 @@ let Objects = {
     semestre1: [],
   },
 };
+function upScreen() {
+  addEventListener("change", function (){
+    this.document.querySelector(".dropdown1")
+  }); 
+}
+upScreen();
 //Loop for index
 for (var i = 1; i <= 5; i++) {
   getButton(i);
 }
-//Main function
+//adds row columns and innerHTML
+function addRow(index, tablo, grade, descri) {
+  if (index == 3 || index === 4) {
+  let row = tablo.insertRow(0);
+  let cell1 = row.insertCell(0);
+  cell1.innerHTML = grade;
+  }else{
+    let row = tablo.insertRow(0);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    cell1.innerHTML = grade;
+    cell2.innerHTML = descri;
+  }
+}
+//Get and push grade in the right table
 function getButton(index) {
   let inputs = document.getElementById("addButton-" + index);
   inputs.addEventListener("click", function () {
@@ -40,32 +60,29 @@ function getButton(index) {
     if (grade > 6) {
       alert("Votre note n'est pas valable.");
       return 0;
-    }
-    if (!isNaN(grade)) {
+    }if (!isNaN(grade)) {
       if (index == 3 || index === 4) {
         let sems = document.getElementById("dropdown-" + index).value;
-        let tablo = document.getElementById(sems);
-        Objects["tabl" + index][sems].push(grade);
-        console.log(Objects["tabl" + index][sems]);
-        let row = tablo.insertRow(0);
-        let cell1 = row.insertCell(0);
-        cell1.innerHTML = grade;
+        let tablo = document.getElementById("tabl" + index);
+        Notes["tabl" + index][sems].push(grade);
+        console.log(Notes["tabl" + index][sems]);
+        addRow(index, tablo, grade)
+        console.log(Object.values(Notes));
       } else {
-        let descri = parseFloat(document.getElementById("descri-" + index).value);
-        let tablo = document.getElementById("tablo-" + index);
-        Objects["tabl" + index].semestre1.push(grade);
-        console.log(Objects["tabl" + index].semestre1);
-        let row = tablo.insertRow(0);
-        let cell1 = row.insertCell(0);
-        let cell2 = row.insertCell(1);
-        cell1.innerHTML = grade;
-        cell2.innerHTML = descri;
+        let descri = document.getElementById("descri-" + index).value;
+        let tablo = document.getElementById("tabl" + index);
+        Notes["tabl" + index].semestre1.push(grade);
+        addRow(index, tablo, grade, descri)
       }
     } else {
       alert("Votre note n'est pas valable.");
     }
   });
 }
+
+//grade has to = Notes note and add all notes in new rowss
+// });
+// }
 // function getTablo(index) {
 // var table = document.getElementById("tablo" + index);
 // var row = table.insertRow([0]);
@@ -77,8 +94,8 @@ function getButton(index) {
 //   button.addEventListener('click', (e) => {
 //     const inputs = e.currentTarget.parentElement.parentElement.querySelectorAll('input')
 //   })}
-// s.push(Object.keys(Objects.tabl + index))
-// console.log(Object.keys(Objects.tabl + index));
+// s.push(Object.keys(Notes.tabl + index))
+// console.log(Object.keys(Notes.tabl + index));
 /* <div id="testBN"></div>;
 
 function updateViewFromDatas(data) {
